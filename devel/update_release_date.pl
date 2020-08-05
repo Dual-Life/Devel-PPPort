0921c35af43f8cd833b9534f1cbfc4eee920874b
Nicolas R <atoomic@cpan.org>
2020-08-05 15:39:24 -0600

    Setup D_PPP_RELEASE_DATE on dist
    When running make regen_release_date which
    is trigger as part of make regen or make dist
    we are updating parts/inc/version with the
    current timestamp.

---
+++ devel/update_release_date.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+exit( run(@ARGV) || 0 ) unless caller;
+
+sub run {
+    my $now = time();
+    my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) =
+      localtime($now);
+    ++$mon;
+    $year += 1900;
+
+    my $human_ts = sprintf( "%04d-%02d-%02d", $year, $mon, $mday );
+
+    print qq[# Updating D_PPP_RELEASE_DATE timestamp to $now /* $human_ts */\n];
+
+    my $f             = q[parts/inc/version];
+    my $file_to_patch = $FindBin::Bin . '/../' . $f;
+    die "Cannot find $f: $!" unless -e $file_to_patch;
+
+    my $content;
+    {
+        local $/;
+        open( my $fh, '<', $file_to_patch );
+        $content = <$fh>;
+    }
+
+    $content =~
+      s{^(\#\s*define\s+D_PPP_RELEASE_DATE)\b.*$}{$1 $now /* $human_ts */}m
+      or die "Cannot find D_PPP_RELEASE_DATE pattern in file $f";
+
+    {
+        local $/;
+        open( my $fh, '>', $file_to_patch );
+        print {$fh} $content;
+    }
+
+    print qq[$f patched with D_PPP_RELEASE_DATE\n];
+
+    return;
+}
+
+1;
